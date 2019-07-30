2ef570fd1391d7f0925f25d5fa9c59ffd69c1ce2
pixeltris <pixeltris@users.noreply.github.com>
2019-07-30 03:02:40 +0100

    Add the proxy server

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# ww32vmt
- win32 utility for x86 web emulators (v86, JSLinux, DOSBox)
+# ww32vmt (web win32 virtual machine tools)
+
+This is a utility program which provides partial internet access, file transfer capabilites and clipboard access on Windows virtual machines implemented within a web browser (v86, JSLinux, DOSBox). It works on Windows 95, Windows 98, and Windows 2000 guest operating systems.
+
+It works by searching for specific blocks in the memory of the guest OS, then using those blocks of memory as a way of communicating between JavaScript in the win32 tool coded in C. This method of communication means that very little needs to be known about the internals of the emulator, and no direct modifications of the emulator are required (other than getting access to the OS memory in the global scope of JavaScript).
+
+One drawback of this approach is that as the OS memory gets more fragmented it becomes less likely that it will be able to find the desired blocks of memory. With some additional knowledge of the internal layout of memory it might be possible to ensure a 100% success rate of finding the blocks.
+
+## Internet
+
+As vmtools.exe is a simple usermode win32 program it can't provide full internet access. Instead it provides simple proxy servers (SOCKS / HTTP / DNS) which can be used in combination with Internet Explorer config and tools suchs as SocksCap. `WvSocksServer` implements the proxy servers which JavaScript connects to via the WebSocket API.
+
+There is an additional HTTP proxy server implementation which supports direct HTTP requests in JavaScript via XMLHttpRequest. This avoids having to run a proxy server to forward requests. One drawback of this type of proxy is that it's subject to the limitations of [CORS](https://en.wikipedia.org/wiki/Cross-origin_resource_sharing), therefore you can pretty much limited to accessing sites explicitly flagged as `Access-Control-Allow-Origin: *`.
+
+NOTE: Internet access depends on TCP/IP being installed (and maybe Winsock 2+)
+
+### IP/ports used in the VM
+
+- HTTP - localhost:80
+- HTTPS - localhost:443 (doesn't work on Internet Explorer?)
+- DNS - localhost:53
+- HTTP (XMLHttpRequest) - localhost:980
+- HTTPS (XMLHttpRequest) - localhost:9443 (this currently isn't implemented as it requires manually handling TLS/SSL)
+
+### Ports used by proxy servers (WebSocket - currently set up to be ws:// rather than wss://)
+
+- SOCKS 861
+- DNS 862
+- HTTP 863
+- HTTPS 864
+
+### Ports used by the WvSocksServer implementation of the proxy servers
+
+When a connection is made to a WebSocket server, it then creates a client to connect to one of the following locally hosted proxy servers (which handles the actual proxy connection). DNS is simple enough to just forward the requests directly.
+
+- SOCKS 961
+- HTTP 962
+- HTTPS 963
+
+## Clipboard
+
+`getCB` can be used to get the current clipboard text from the VM. When `getCB` is a button, you can press it to fetch the VM clipboard contents. When it's a text box you need to use the 'copy' keyboard shortcut which will request the text from the VM (a further shortcut may need to be pressed if the textbox says "OK").
+
+`setCB` can be used to set the VM clipboard text. When `setCB` is a button, you can press it to set the VM clipboard contents. When it's a text box you need to use the 'paste' keyboard shortcut to set the VM clipboard contents. NOTE: This doesn't automatically invoke a 'paste' inside the VM, you will still need to do that manually.
+
+## File transfer
+
+The `download` button can be used to fetch the contents of a folder named `Shared` next to vmtools.exe, and download it as a zip/tar.
+
+The `upload` button can be used to transfer one or more files to VM. If you chose exactly one `.zip` or `.tar` file then it will automatically extract the archive when sending it to the VM.
+
+- NOTE: vmtools may freeze while files are being transferred. You can view the developer tools console (`F12` / `Ctrl+Shift+J`) which will write a log each time a file is being transferred.
+- NOTE: This all happens locally. None of this requires communication with a server.
---
+++ WvSocksServer/WvSocksServer/Program.cs
@@ -0,0 +1,255 @@
+﻿using Fleck;
+using Mentalis.Proxy.Socks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace WvSocksServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sockets
+            const int socksPort = 961;
+            const int httpPort = 962;
+            const int httpsPort = 963;
+            // websockets
+            const int webSockPort = 861;
+            const int webSockDnsPort = 862;
+            const int webSockHttpPort = 863;
+            const int webSockHttpsPort = 864;
+            int dnsTimeout = 30000;
+            string wsUrl = "ws://0.0.0.0";
+
+            Dictionary<int, int> simpleTcpProxyPorts = new Dictionary<int, int>()
+            {
+                { webSockPort, socksPort },
+                { webSockHttpPort, httpPort },
+                { webSockHttpsPort, httpsPort },
+            };
+            // This is mostly to keep the WebSocketServer objects alive (though they might be rooted via the sockets anyway?)
+            Dictionary<int, WebSocketServer> simpleTcpProxies = new Dictionary<int, WebSocketServer>();
+
+            IPAddress dnsAddress = GetDnsAddress();
+
+            SocksListener socksServer = new SocksListener(IPAddress.Any, socksPort);
+            socksServer.Start();
+
+            Mentalis.Proxy.Http.HttpListener httpServer = new Mentalis.Proxy.Http.HttpListener(IPAddress.Any, httpPort);
+            httpServer.Start();
+
+            Mentalis.Proxy.Http.HttpListener httpsServer = new Mentalis.Proxy.Http.HttpListener(IPAddress.Any, httpsPort);
+            httpsServer.Start();
+
+            WebSocketServer webSocketDnsServer = new WebSocketServer(wsUrl + ":" + webSockDnsPort);
+            webSocketDnsServer.Start(webSocket =>
+            {
+                bool closed = false;
+                webSocket.OnOpen = () =>
+                {
+                    Console.WriteLine("Connected (DNS)");
+                };
+                webSocket.OnClose = () =>
+                {
+                    closed = true;
+                    Console.WriteLine("Disconnected (DNS)");
+                };
+                webSocket.OnBinary = (byte[] data) =>
+                {
+                    try
+                    {
+                        int addrLen = BitConverter.ToInt32(data, 0) + 4;
+                        byte[] addrBytes = new byte[addrLen];
+                        Buffer.BlockCopy(data, 0, addrBytes, 0, addrBytes.Length);
+
+                        byte[] dnsReq = new byte[data.Length - addrLen];
+                        Buffer.BlockCopy(data, addrLen, dnsReq, 0, dnsReq.Length);
+
+                        UdpClient client = new UdpClient();
+                        client.Connect(dnsAddress, 53);
+                        client.Send(dnsReq, dnsReq.Length);
+
+                        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 53);
+                        byte[] response = client.Receive(ref remoteEndPoint);
+                        
+                        byte[] responseBuffer = new byte[addrBytes.Length + response.Length];
+                        Buffer.BlockCopy(addrBytes, 0, responseBuffer, 0, addrBytes.Length);
+                        Buffer.BlockCopy(response, 0, responseBuffer, addrLen, response.Length);
+                        webSocket.Send(responseBuffer);
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            closed = true;
+                            webSocket.Close();
+                        }
+                        catch
+                        {
+                        }
+                    }
+                };
+                ThreadPool.QueueUserWorkItem((object o) =>
+                {
+                    DateTime time = DateTime.Now;
+                    while (!closed)
+                    {
+                        Thread.Sleep(1000);
+                        if (DateTime.Now - TimeSpan.FromMilliseconds(dnsTimeout) > time)
+                        {
+                            try
+                            {
+                                webSocket.Close();
+                            }
+                            catch
+                            {
+                            }
+                            break;
+                        }
+                    }
+                });
+            });
+
+            foreach (KeyValuePair<int, int> proxy in simpleTcpProxyPorts)
+            {
+                WebSocketServer webSocketServer = new WebSocketServer(wsUrl + ":" + proxy.Key);
+                simpleTcpProxies[proxy.Key] = webSocketServer;
+                webSocketServer.Start(webSocket =>
+                {
+                    TcpClient client = new TcpClient();
+                    Thread clientThread = null;
+                    try
+                    {
+                        client.Connect(IPAddress.Loopback, proxy.Value);
+                    }
+                    catch
+                    {
+                        Close(client, webSocket, clientThread);
+                    }
+
+                    webSocket.OnOpen = () =>
+                    {
+                        Console.WriteLine("Connected");
+                        clientThread = new Thread(delegate ()
+                        {
+                            try
+                            {
+                                byte[] buff = new byte[4096];
+                                while (client.Connected)
+                                {
+                                    int readBytes = client.Client.Receive(buff);
+                                    if (readBytes > 0)
+                                    {
+                                        Console.WriteLine("Send " + readBytes);
+                                        byte[] temp = new byte[readBytes];
+                                        Buffer.BlockCopy(buff, 0, temp, 0, temp.Length);
+                                        webSocket.Send(temp);
+                                    }
+                                    else
+                                    {
+                                        Close(client, webSocket, clientThread);
+                                        break;
+                                    }
+                                }
+                            }
+                            catch
+                            {
+                                Close(client, webSocket, clientThread);
+                            }
+                        });
+                        try
+                        {
+                            clientThread.Start();
+                        }
+                        catch
+                        {
+                            Close(client, webSocket, clientThread);
+                        }
+                    };
+
+                    webSocket.OnClose = () =>
+                    {
+                        Console.WriteLine("Disconnected");
+                        Close(client, webSocket, clientThread);
+                    };
+                    webSocket.OnBinary = (byte[] data) =>
+                    {
+                        try
+                        {
+                            Console.WriteLine("Recv " + data.Length);
+                            if (client.Client.Send(data) != data.Length)
+                            {
+                                Console.WriteLine("TODO: Better handling of data (send)");
+                            }
+                        }
+                        catch
+                        {
+                            Close(client, webSocket, clientThread);
+                        }
+                    };
+                });
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void Close(TcpClient client, IWebSocketConnection webSocket, Thread clientThread)
+        {
+            try
+            {
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+            catch
+            {
+            }
+            try
+            {
+                if (webSocket != null)
+                {
+                    webSocket.Close();
+                }
+            }
+            catch
+            {
+            }
+            if (clientThread != null)
+            {
+                try
+                {
+                    clientThread.Abort();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        static IPAddress GetDnsAddress()
+        {
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface networkInterface in networkInterfaces)
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
+                    IPAddressCollection dnsAddresses = ipProperties.DnsAddresses;
+                    foreach (IPAddress dnsAdress in dnsAddresses)
+                    {
+                        return dnsAdress;
+                    }
+                }
+            }
+            throw new InvalidOperationException("Unable to find DNS Address");
+        }
+    }
+}
